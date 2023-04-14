024d6ff2804e564a3a58966b78e3c509c4e13d0c
Devfrank94 <francescomurro94@gmail.com>
2023-04-14 15:43:59 +0200

    completed milestone 2

--- js/script.js
+++ js/script.js
@@ -11,14 +11,44 @@ createApp({
         },
         {
           text: 'Cambiare olio motore auto',
-          check: true
+          check: false
         },
         {
           text: 'Ripassare Javascript',
           check: false
         },
-      ]
+      ],
+
+      errorMsg:''
+    }
+  },
+
+
+methods:{
+
+  remtsk(index){
+    if(this.tasks[index].check){
+      this.tasks.splice(index,1)
+      this.errorMsg = '';
+    }else{
+      this.errorMsg = 'La task può essere eliminata solo se completata'
     }
   }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 }).mount("#app")
